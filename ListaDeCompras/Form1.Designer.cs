4ede18968d69355da2f98d43a1d1a1b22a7356cc
Orca <thiago.tacarval@gmail.com>
2025-08-15 21:54:21 -0300

    validaçao de erros e implementaçao de dados
    validaçao de erros para campos vazios oi invalidos  e os recursos adicionar, excluir e limpar.

--- ListaDeCompras/Form1.Designer.cs
+++ ListaDeCompras/Form1.Designer.cs
@@ -28,13 +28,122 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.lblProduto = new System.Windows.Forms.Label();
+            this.txbProduto = new System.Windows.Forms.TextBox();
+            this.btnAdc = new System.Windows.Forms.Button();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.listCompras = new System.Windows.Forms.ListView();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("TechnicBold", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.lblTitulo.Location = new System.Drawing.Point(355, 103);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(309, 38);
+            this.lblTitulo.TabIndex = 1;
+            this.lblTitulo.Text = "Lista de Compras";
+            this.lblTitulo.Click += new System.EventHandler(this.lblTitulo_Click);
+            // 
+            // lblProduto
+            // 
+            this.lblProduto.AutoSize = true;
+            this.lblProduto.Font = new System.Drawing.Font("TechnicBold", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.lblProduto.Location = new System.Drawing.Point(240, 144);
+            this.lblProduto.Name = "lblProduto";
+            this.lblProduto.Size = new System.Drawing.Size(158, 20);
+            this.lblProduto.TabIndex = 2;
+            this.lblProduto.Text = "Nome do Produto:";
+            // 
+            // txbProduto
+            // 
+            this.txbProduto.Location = new System.Drawing.Point(404, 144);
+            this.txbProduto.Name = "txbProduto";
+            this.txbProduto.Size = new System.Drawing.Size(100, 22);
+            this.txbProduto.TabIndex = 3;
+            this.txbProduto.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txbProduto_KeyDown);
+            // 
+            // btnAdc
+            // 
+            this.btnAdc.Font = new System.Drawing.Font("TechnicBold", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.btnAdc.Location = new System.Drawing.Point(510, 144);
+            this.btnAdc.Name = "btnAdc";
+            this.btnAdc.Size = new System.Drawing.Size(154, 23);
+            this.btnAdc.TabIndex = 4;
+            this.btnAdc.Text = "ADICIONAR";
+            this.btnAdc.UseVisualStyleBackColor = true;
+            this.btnAdc.Click += new System.EventHandler(this.btnAdc_Click);
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.Font = new System.Drawing.Font("TechnicBold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.btnExcluir.Location = new System.Drawing.Point(303, 372);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(302, 41);
+            this.btnExcluir.TabIndex = 6;
+            this.btnExcluir.Text = "Excluir Item";
+            this.btnExcluir.UseVisualStyleBackColor = true;
+            this.btnExcluir.Click += new System.EventHandler(this.btnExcluir_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ListaDeCompras.Properties.Resources.Rafiqul_Hassan_Blogger_Basket_512;
+            this.pictureBox1.Location = new System.Drawing.Point(278, 87);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(71, 54);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // listCompras
+            // 
+            this.listCompras.HideSelection = false;
+            this.listCompras.Location = new System.Drawing.Point(303, 172);
+            this.listCompras.Name = "listCompras";
+            this.listCompras.Size = new System.Drawing.Size(302, 194);
+            this.listCompras.TabIndex = 8;
+            this.listCompras.Tag = "";
+            this.listCompras.UseCompatibleStateImageBehavior = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.ClientSize = new System.Drawing.Size(933, 484);
+            this.Controls.Add(this.listCompras);
+            this.Controls.Add(this.btnExcluir);
+            this.Controls.Add(this.btnAdc);
+            this.Controls.Add(this.txbProduto);
+            this.Controls.Add(this.lblProduto);
+            this.Controls.Add(this.lblTitulo);
+            this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("TechnicBold", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Lista de Compras:";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Label lblProduto;
+        private System.Windows.Forms.TextBox txbProduto;
+        private System.Windows.Forms.Button btnAdc;
+        private System.Windows.Forms.Button btnExcluir;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ListView listCompras;
     }
 }
 
--- ListaDeCompras/Form1.cs
+++ ListaDeCompras/Form1.cs
@@ -16,5 +16,49 @@ namespace ListaDeCompras
         {
             InitializeComponent();
         }
+
+        private void lblTitulo_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAdc_Click(object sender, EventArgs e)
+        {
+            if (txbProduto.Text.Length == 0)
+            {
+                MessageBox.Show("O nome do produto não pode estar vazio", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if (txbProduto.Text.Length < 2)
+            {
+                MessageBox.Show("O nome do produto precisa ter no  minimo duas letras!", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                listCompras.Items.Add(txbProduto.Text);
+                    MessageBox.Show($"{txbProduto.Text} foi adicionado na lista de compras ! ", "show!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                    txbProduto.Text = ""; //igual txbProduto.Clear();
+            }
+        }
+
+        private void btnExcluir_Click(object sender, EventArgs e)
+        {
+            DialogResult resposta = MessageBox.Show("Tem certeza que deseja apagar tudo ? ", "atenção", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (resposta == DialogResult.Yes)
+            {
+              listCompras.Items.Clear();
+            }
+
+        }
+
+        private void txbProduto_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                btnAdc.PerformClick();
+            }
+        }
     }
 }
+
